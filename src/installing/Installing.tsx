8cf6058a8a9c518206f636502f0def25ea45661c
Oskar Nizioł <oskarniziol@gmail.com>
2024-01-27 13:34:02 +0100

    show on what disk installation script is running on

--- src/installing/Installing.tsx
+++ src/installing/Installing.tsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-import React from "react";
+import React, {useEffect} from "react";
 import "./installing.css"
 import {BarLoader} from "react-spinners";
 import {useTranslation} from "react-i18next";
+import {useAtomState} from "@zedux/react";
+import {selectedDiskState} from "../state/diskState.ts";
 export default function Installing() {
     const { t } = useTranslation()
+    const [disk] = useAtomState(selectedDiskState)
+
+    useEffect(() => {
+        console.log("Installation script running on ", disk)
+    }, [disk])
 
     return (
         <div className="installing fadein">
